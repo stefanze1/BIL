47dab805f90f32f654f17bca7134fae4d5fb46a8
stefanze1 <stefanze@stud.ntnu.no>
2025-12-18 21:59:47 +0100

    Update LineSensor.cpp

--- src/LineSensor.cpp
+++ src/LineSensor.cpp
@@ -1,6 +1,5 @@
 #include "linesensors.h"
 #include <Arduino.h>
-
 #define NUM_SENSORS 5
 #define maxSpeed 300
 #define maxSpeed2 200
@@ -29,64 +28,148 @@ void calibrateLineSensors() {
     }
     motors.setSpeeds(0, 0);
 }
+bool isLineLost() {
+    static uint8_t lostCounter = 0;
+
+    bool anyDark = false;
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lineSensorValues[i] > 600) {
+            anyDark = true;
+        }
+    }
+
+    if (anyDark) {
+        lostCounter = 0;
+        return false;
+    }
+
+    lostCounter++;
+
+    if (lostCounter >= 3) {
+        lostCounter = 0;
+        return true;
+    }
+
+    return false;
+}
 
 void followLine() {
-    int16_t LSPosition = lineSensors.readLine(lineSensorValues); //Leser posisjon
-    int16_t LSError = LSPosition - 2000; // trekker fra 200, da det er midtpunkt
-    int16_t speedDiff = LSError / 12 + 1 * (LSError - LSLastError); //Dette er PID regulering, uten I. funnet på nett.
+   
+    int16_t LSPosition = lineSensors.readLine(lineSensorValues);
+
+    
+    bool allWhite = true;
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lineSensorValues[i] > 200) {
+            allWhite = false;
+            break;
+        }
+    }
+    
+    if (allWhite && abs(LSLastError) < 700) {  
+       
+        motors.setSpeeds(0, 0);
+        return;
+    }
+
+   
+    int16_t LSError = LSPosition - 2000;
+
+    float Kp = 0.08;
+    float Kd = 0.30;
+
+    float speedDiffF = Kp * LSError + Kd * (LSError - LSLastError);
     LSLastError = LSError;
+    int16_t speedDiff = (int16_t)speedDiffF;
 
-    int16_t LSLeftSpeed = (int16_t)maxSpeed/2 + speedDiff;
-    int16_t LSRightSpeed = (int16_t)maxSpeed/2 - speedDiff;
+    
+    int16_t base = maxSpeed / 2;
 
-    LSLeftSpeed = constrain(LSLeftSpeed, 0, (int16_t)maxSpeed);
-    LSRightSpeed = constrain(LSRightSpeed, 0, (int16_t)maxSpeed);
+    int16_t left = base + speedDiff;
+    int16_t right = base - speedDiff;
 
-    motors.setSpeeds(LSLeftSpeed, LSRightSpeed);
+    left = constrain(left, 0, maxSpeed);
+    right = constrain(right, 0, maxSpeed);
 
-   }
-
+    motors.setSpeeds(left, right);
+}
 void followLine2() {
     int16_t LSPosition = lineSensors.readLine(lineSensorValues);
+
+    bool allWhite = true;
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lineSensorValues[i] > 200) { allWhite = false; break; }
+    }
+
+    if (allWhite && abs(LSLastError) < 700) {
+        motors.setSpeeds(0, 0);
+        return;
+    }
+
     int16_t LSError = LSPosition - 2000;
-    int16_t speedDiff = LSError / 8 + 1 * (LSError - LSLastError);
+
+    float Kp = 0.07;
+    float Kd = 0.28;
+
+    float speedDiffF = Kp * LSError + Kd * (LSError - LSLastError);
     LSLastError = LSError;
+    int16_t speedDiff = (int16_t)speedDiffF;
 
-    int16_t LSLeftSpeed = (int16_t)maxSpeed2/2 + speedDiff;
-    int16_t LSRightSpeed = (int16_t)maxSpeed2/2 - speedDiff;
+    int16_t base = maxSpeed2 / 2;
 
-    LSLeftSpeed = constrain(LSLeftSpeed, 0, (int16_t)maxSpeed2);
-    LSRightSpeed = constrain(LSRightSpeed, 0, (int16_t)maxSpeed2);
+    int16_t left = base + speedDiff;
+    int16_t right = base - speedDiff;
 
-    motors.setSpeeds(LSLeftSpeed, LSRightSpeed);
+    left = constrain(left, 0, maxSpeed2);
+    right = constrain(right, 0, maxSpeed2);
+
+    motors.setSpeeds(left, right);
 }
+
 void followLine3() {
     int16_t LSPosition = lineSensors.readLine(lineSensorValues);
+
+    bool allWhite = true;
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if (lineSensorValues[i] > 200) { allWhite = false; break; }
+    }
+
+    if (allWhite && abs(LSLastError) < 700) {
+        motors.setSpeeds(0, 0);
+        return;
+    }
+
     int16_t LSError = LSPosition - 2000;
-    int16_t speedDiff = LSError / 8 + 1 * (LSError - LSLastError);
+
+    float Kp = 0.06;
+    float Kd = 0.25;
+
+    float speedDiffF = Kp * LSError + Kd * (LSError - LSLastError);
     LSLastError = LSError;
+    int16_t speedDiff = (int16_t)speedDiffF;
 
-    int16_t LSLeftSpeed = (int16_t)maxSpeed3/2 + speedDiff;
-    int16_t LSRightSpeed = (int16_t)maxSpeed3/2 - speedDiff;
+    int16_t base = maxSpeed3 / 2;
 
-    LSLeftSpeed = constrain(LSLeftSpeed, 0, (int16_t)maxSpeed3);
-    LSRightSpeed = constrain(LSRightSpeed, 0, (int16_t)maxSpeed3);
+    int16_t left = base + speedDiff;
+    int16_t right = base - speedDiff;
 
-    motors.setSpeeds(LSLeftSpeed, LSRightSpeed);
+    left = constrain(left, 0, maxSpeed3);
+    right = constrain(right, 0, maxSpeed3);
+
+    motors.setSpeeds(left, right);
 }
 
-
 void returnToCharger () {
-    // int16_t LSPosition = lineSensors.readLine(lineSensorValues);
+    
   
-    if (lineSensorValues[4] > 925 ) { //&& LSPosition > 3400
+    if (lineSensorValues[4] > 925 ) { 
         motors.setSpeeds(0,0);
         delay(50);
         motors.setSpeeds(200,0);
         delay(500);
         motors.setSpeeds(0,0);
     }
-    if (lineSensorValues[0] > 950  ) { //&& LSPosition < 400
+    if (lineSensorValues[0] > 950  ) { 
         motors.setSpeeds(0,0);
         delay(50);
         motors.setSpeeds(0,200);
@@ -94,38 +177,3 @@ void returnToCharger () {
         motors.setSpeeds(0,0);
     }
 }
-  
-
-//Stoppe ved dead end
-    // int16_t LSPosition = lineSensors.readLine(lineSensorValues);
-    
-    // bool deadEnd = true;
-    // for (int i = 0; i < 5; i++) {
-    //     if (lineSensorValues[i] > 25) {
-    //         deadEnd = false;
-    //         break;
-    //     }
-    // }
-
-    // if (deadEnd) {
-    //     motors.setSpeeds(0,0);
-    //     delay(1000);
-    //     return;
-    //     }
-
-
-
-
-
-// void proxSensor() {
-//     proxSensors.read();
-//     proxSensors.read();
-
-//     int8_t leftProx = proxSensors.countsFrontWithLeftLeds();
-//     int8_t rightProx = proxSensors.countsFrontWithRightLeds();
-
-//     if (leftProx > 5 || rightProx > 5) { // 0 til 7 
-//         motors.setSpeeds(0,0);
-//         delay(10000);
-//     }     
-// }
