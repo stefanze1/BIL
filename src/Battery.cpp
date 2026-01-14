c3d386124d6c31182a7f79945ef9df15014b7529
Your Name <you@example.com>
2026-01-14 09:16:07 +0100

    Fungerende batteri

--- src/Battery.cpp
+++ src/Battery.cpp
@@ -11,7 +11,7 @@ int leftTickCount = 0;
 float batteryLevel = 100;
 float newBatteryLevel = 0.00;
 
-float batteryDrain = 0.005;
+float batteryDrain = 0.05;
 
 
 Zumo32U4Motors motors;
@@ -23,22 +23,8 @@ void battery() {
     rightTick = encoders.getCountsAndResetRight();
     leftTick = encoders.getCountsAndResetLeft();
 
-    
-/*
-    while (batteryLevel > 0) {
-        if (rightTick == 1 || leftTick == 1) {        
-        newBatteryLevel = (batteryLevel - 0.005);
-        batteryLevel = newBatteryLevel;
-        Serial.print(batteryLevel);
-        Serial.print("  ");
-        }
-        else {
-            break;
-        }
 
-    }*/
-
-    while (batteryLevel > 0) {
+    if (batteryLevel > 0) {
         rightTick = encoders.getCountsAndResetRight();
         leftTick = encoders.getCountsAndResetLeft();
 
@@ -48,8 +34,7 @@ void battery() {
         }
 
         if (rightTickCount > 12) {
-            newBatteryLevel = (batteryLevel - batteryDrain);
-            batteryLevel = newBatteryLevel;
+            batteryLevel -= batteryDrain;
             rightTickCount = 0;
 
             Serial.print(batteryLevel);
@@ -61,8 +46,7 @@ void battery() {
         }
 
          if (rightTickCount > 12) {
-            newBatteryLevel = (batteryLevel - batteryDrain);
-            batteryLevel = newBatteryLevel;
+            batteryLevel -= batteryDrain;
             leftTickCount = 0;
 
             Serial.print(batteryLevel);
@@ -73,3 +57,4 @@ void battery() {
     }
 
 }
+
