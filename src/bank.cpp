035783c51e16e511b822078a143ab600897d7c86
Your Name <you@example.com>
2026-01-28 09:58:25 +0100

    batteri med fungerende og skjerm samt førsteutkast pengesystem

--- src/bank.cpp
+++ src/bank.cpp
@@ -13,10 +13,8 @@ void updateBalance(){
     int coffeeprice = 19;
 
     int ticketssold = random(0,5);
-    delay(200);
     int coffeesSold = random(0,ticketssold);
-    delay(200);
+    
     currentBalance = (coffeesSold * coffeeprice +  ticketssold * ticketprice );
     balance += currentBalance;
-    Serial.println(balance);
 }
